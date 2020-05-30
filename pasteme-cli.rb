de617fabb45f848e1b16e161e4bb7261ebd8e663
Richard Szolár <r@richardszolar.me>
2020-05-30 20:32:16 +0200

    Update pasteme-cli.rb

--- pasteme-cli.rb
+++ pasteme-cli.rb
@@ -3,14 +3,14 @@ class PastemeCli < Formula
   
   desc "CLI client for the Paste.me service"
   homepage "https://paste.me"
-  v tag
+  version tag
   depends_on :arch => :x86_64
   
   if OS.mac?
-    url "https://github.com/0x111/pasteme-cli/releases/download/#{v}/pasteme-cli-darwin-amd64"
+    url "https://github.com/0x111/pasteme-cli/releases/download/#{version}/pasteme-cli-darwin-amd64"
     sha256 "0b8ee6adfa833b4a54168e49400ff71553c53119f00710913de5af0370a16d68"
   elsif OS.linux?
-    url "https://github.com/0x111/pasteme-cli/releases/download/#{v}/pasteme-cli-linux-amd64"
+    url "https://github.com/0x111/pasteme-cli/releases/download/#{version}/pasteme-cli-linux-amd64"
     sha256 "ed3bbb6758b624d8d025229e6593f3c76562d059dacfbaf61befd1fa10dd249d"
   end
   
